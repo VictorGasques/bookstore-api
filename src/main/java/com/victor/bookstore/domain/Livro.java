7a137a0ea2f1385930c0e9efe89c4a7a91bc2af5
VictorGasques <victorgasques10@hotmail.com>
2022-07-18 11:54:24 -0300

    classes de dominio iniciadas

---
+++ src/main/java/com/victor/bookstore/domain/Livro.java
@@ -0,0 +1,13 @@
+package com.victor.bookstore.domain;
+
+public class Livro {
+	
+	private Integer id;
+	private String titulo;
+	private String nome_autor;
+	private String texto;
+	
+	private Categoria categoria;
+	
+
+}
